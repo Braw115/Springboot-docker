56a02f4ed3f202a9b972e691e38acd6d15ab92dc
brawforever <66543232+brawforever@users.noreply.github.com>
2022-04-03 18:01:01 +0800

    test conflict

--- pitaka-web/src/main/java/com/pitaka/www/pitakaController/UserController.java
+++ pitaka-web/src/main/java/com/pitaka/www/pitakaController/UserController.java
@@ -39,6 +39,7 @@ public class UserController {
     public void test(){
         System.out.println("test...");
         System.out.println("test for brawforever");
+        System.out.println("test conflict");
     }
 
 
